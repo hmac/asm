883f3a5082e4559040fb72e978ecd4f01e180d20
Harry Maclean <harry@hmac.dev>
2023-01-01 12:17:35 +1300

    Add nodejs

--- shell.nix
+++ shell.nix
@@ -1,4 +1,9 @@
-{pkgs ? (import <nixpkgs> {}), ...}:
-  pkgs.mkShell {
-    buildInputs = with pkgs; [nasm llvm];
-  }
+{ pkgs ? (import <nixpkgs> { }), ... }:
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    nasm
+    llvm
+    # nodejs 16.x is required for copilot
+    nodejs-16_x
+  ];
+}
